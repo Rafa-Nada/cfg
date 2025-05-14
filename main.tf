eb237f38c4998b939968e5eed8d5f4f0f25751c7
Rafa-Nada <66781988+Rafa-Nada@users.noreply.github.com>
2025-05-14 17:10:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,90 +2,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = "appgw-rg"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "appgw-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# Create an Azure Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "harnesseaccount" # Storage account name (must be globally unique)
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_kind             = "StorageV2" # Or "BlobStorage", "FileStorage", etc.
+  account_replication_type = "LRS"  # Or "GRS", "ZRS", etc.
+  # Optional: Enable static website hosting
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "appgw-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "appgw-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_application_gateway" "appgw" {
-  name                = "example-appgw"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "appgw-ip-config"
-    subnet_id = azurerm_subnet.subnet.id
-  }
-
-  frontend_port {
-    name = "appgw-fe-port"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "appgw-fe-ip"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
-  }
-
-  backend_address_pool {
-    name = "appgw-be-pool"
-    backend_addresses {
-      ip_address = "10.0.2.4"
-    }
-  }
-
-  backend_http_settings {
-    name                  = "appgw-http-settings"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 20
-  }
-
-  http_listener {
-    name                           = "appgw-listener"
-    frontend_ip_configuration_name = "appgw-fe-ip"
-    frontend_port_name             = "appgw-fe-port"
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = "appgw-rule"
-    rule_type                  = "Basic"
-    http_listener_name         = "appgw-listener"
-    backend_address_pool_name  = "appgw-be-pool"
-    backend_http_settings_name = "appgw-http-settings"
-  }
-
-  tags = {
-    environment = "demo"
-  }
+  enable_http_logging = true
+  # Optional: Enable large file support
+  enable_large_file_support = true
 }
