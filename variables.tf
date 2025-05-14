61ec5b9c00fc9fbf9c4a53ba8036e881807b1971
Rafa-Nada <66781988+Rafa-Nada@users.noreply.github.com>
2025-05-14 16:37:21 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "appgw_name" {
+  type    = string
+  default = "example-appgw"
+}
+
+variable "sku_name" {
+  type    = string
+  default = "WAF_v2" # Change to "Standard_v2" if not using WAF
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "WAF_v2"
+}
+
+variable "sku_capacity" {
+  type    = number
+  default = 2
+}
+
+#variable "ssl_certificate_data" {
+ # type = string
+#}
+
+#variable "ssl_certificate_password" {
+#  type = string
+#}
